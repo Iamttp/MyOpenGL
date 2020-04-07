a6a1be355862b583e9b458ed8a8af49a8cbd6c7c
iamttp <1877784332@qq.com>
2020-04-07 21:00:22 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <GL/glut.h>
 #include <cmath>
+#include <iostream>
 #include <ctime>
+#include <vector>
 #include "myDrawUtil.h"
-#include "snake.h"
 
 static float angle = 0.0, ratio;  // angle绕y轴的旋转角，ratio窗口高宽比
-static float x = 0.0f, y = 0.0f, z = 3.0f;  //相机位置
+static float x = 0.0f, y = 0.0f, z = 6.0f;  //相机位置
 static float lx = 0.0f, ly = 0.0f, lz = -1.0f;  //视线方向，初始设为沿着Z轴负方向
 
 bool mouseDown = false;
 float xrot = 0.0f, yrot = 0.0f;
 float xdiff = 0.0f, ydiff = 0.0f;
 
-float zoom = 0.1f;
-int nowDir = -1;
-snake mySnake(4);
-MyPos<int> myFood;
+const float zoom = 0.1f;
+const int width = 100;
+const int height = 100;
+
+int useUtil = 0;
+std::list<Per3dObject> glp; // 全局已完成的图形
+Per3dObject now;    // 正在绘制的图形
 
 /**
  * 定义观察方式
@@ -61,9 +65,22 @@ void moveMeFlat(int direction) {
 */
 void mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        mouseDown = true;
-        xdiff = x - yrot;
-        ydiff = -y + xrot;
+        if (useUtil == 1) {
+            std::vector<double> world = screen2world(x, y);
+//            std::cout << func(world[1]) << " " << func(world[2]) << " " << myRound(world[3]) << std::endl;
+            now.sketch.emplace_back(myRound(world[1]), myRound(world[2]), world[3]);
+        } else {
+            mouseDown = true;
+            xdiff = x - yrot;
+            ydiff = -y + xrot;
+        }
+    } else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+        if (useUtil == 2) {
+            useUtil = 0;
+            glp.push_back(now);
+            now.sketch.clear();
+            now.h = 0;
+        }
     } else
         mouseDown = false;
 }
@@ -72,6 +89,11 @@ void mouse(int button, int state, int x, int y) {
  * 鼠标移动事件
  */
 void mouseMotion(int x, int y) {
+    if (useUtil == 2) {
+        std::vector<double> world = screen2world(x, y);
+        now.h = world[2];
+        glutPostRedisplay();
+    }
     if (mouseDown) {
         yrot = x - xdiff;
         xrot = y + ydiff;
@@ -84,37 +106,32 @@ void mouseMotion(int x, int y) {
  */
 void processSpecialKeys(int key, int x, int y) {
     switch (key) {
-//        case 'A':
-//            angle -= 0.01f;
-//            orientMe(angle);
-//            break;
-//        case 'D':
-//            angle += 0.01f;
-//            orientMe(angle);
-//            break;
         case GLUT_KEY_PAGE_DOWN:
             moveMeFlat(-1);
             break;
         case GLUT_KEY_PAGE_UP:
             moveMeFlat(1);
             break;
-        case GLUT_KEY_RIGHT:
-            if (nowDir != 1) nowDir = 0;
+        default:
             break;
-        case GLUT_KEY_LEFT:
-            if (nowDir != 0) nowDir = 1;
+    }
+}
+
+void processNormalKeys(unsigned char key, int x, int y) {
+    useUtil = 0;
+    switch (key) {
+        case 'z':
+            // 画笔
+            useUtil = 1;
+            xrot = 0.0f, yrot = 0.0f;
             break;
-        case GLUT_KEY_UP:
-            if (nowDir != 3) nowDir = 2;
+        case 'q':
+            // 拉升
+            useUtil = 2;
+            xrot = -45.0f, yrot = 45.0f;
             break;
-        case GLUT_KEY_DOWN:
-            if (nowDir != 2) nowDir = 3;
-            break;
-        case GLUT_KEY_HOME:
-            if (nowDir != 5) nowDir = 4;
-            break;
-        case GLUT_KEY_END:
-            if (nowDir != 4) nowDir = 5;
+        case 'a':
+            // 切除
             break;
         default:
             break;
@@ -124,59 +141,87 @@ void processSpecialKeys(int key, int x, int y) {
 void myDisplay() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-
     gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 
     // 实现鼠标旋转的核心
     glRotatef(xrot, 1.0f, 0.0f, 0.0f);
     glRotatef(yrot, 0.0f, 1.0f, 0.0f);
 
-    // 创建透视效果视图
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(90.0f, 1.0f, 1.0f, 20.0f);
-    glMatrixMode(GL_MODELVIEW);
-
-    // 平面方块铺设
+    // 最后画草图
     glPushMatrix();
-    glTranslatef(-25 * zoom, -25 * zoom, 0);
-    for (int i = 0; i < 50; i++) {
+    glColor4f(0, 0, 0, 0);
+    drawSketch(now.sketch);
+    glPopMatrix();
+
+    if (useUtil == 2) {
         glPushMatrix();
-        for (int j = 0; j < 50; j++) {
-            drawBox(zoom / 2, zoom / 2, zoom / 2, (i & 1) * 0.2, 0.2, (j & 1) * 0.2, 0.1f);
-            glTranslatef(0, zoom, 0);
-        }
+        glColor4f(0.9, 0.9, 0.9, 0);
+        drawPull(now.sketch, now.h);
         glPopMatrix();
+        glPushMatrix();
+        glColor4f(0, 0, 0, 0);
+        drawPull2(now.sketch, now.h);
+        glPopMatrix();
+    }
+
+    // 然后画已完成
+    for (auto &item:glp) {
+        glPushMatrix();
+        glColor4f(0.7, 0.7, 0.7, 0);
+        drawPull(item.sketch, item.h);
+        glPopMatrix();
+        glPushMatrix();
+        glColor4f(0, 0, 0, 0);
+        drawPull2(item.sketch, item.h);
+        glPopMatrix();
+    }
+
+    // 然后坐标系
+    glLineWidth(5);
+    glEnable(GL_LINE_SMOOTH);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
+    glPushMatrix();
+    glColor4f(1, 0, 0, 0);
+    glBegin(GL_LINES);
+    glVertex3f(-width * zoom, 0, 0);
+    glVertex3f(width * zoom, 0, 0);
+    glEnd();
+
+    glPushMatrix();
+    glColor4f(1, 0, 0, 0);
+    glBegin(GL_LINES);
+    glVertex3f(0, -height * zoom, 0);
+    glVertex3f(0, height * zoom, 0);
+    glEnd();
+
+    // 再画线
+    glPushMatrix();
+    glTranslatef(-width * zoom, 0, 0);
+    glColor4f(0, 1, 0, 0);
+    for (int i = 0; i < 2 * width; i++) {
+        glBegin(GL_LINES);
+        glVertex3f(0, -height * zoom, 0);
+        glVertex3f(0, height * zoom, 0);
+        glEnd();
         glTranslatef(zoom, 0, 0);
     }
     glPopMatrix();
 
-    // 贪吃蛇
-    for (auto &item:mySnake.s) {
-        glPushMatrix();
-        glTranslatef(item.x * zoom, item.y * zoom, item.z * zoom);
-        drawBox(zoom / 2, zoom / 2, zoom / 2, 1.0f, 1.0f, 0, 1);
-        glPopMatrix();
+    glPushMatrix();
+    glTranslatef(0, -height * zoom, 0);
+    for (int i = 0; i < 2 * height; i++) {
+        glBegin(GL_LINES);
+        glVertex3f(-width * zoom, 0, 0);
+        glVertex3f(width * zoom, 0, 0);
+        glEnd();
+        glTranslatef(0, zoom, 0);
     }
-
-    // 食物
-    glPushMatrix();
-    glTranslatef(myFood.x * zoom, myFood.y * zoom, myFood.z * zoom);
-    drawBox(zoom / 2, zoom / 2, zoom / 2, 1.0f, 0, 0, 1);
     glPopMatrix();
 
-    glPushMatrix();
-    std::list<MyPos<float >> lt;
-    lt.push_back({zoom * 2, zoom * 2, zoom * 20});
-    lt.push_back({zoom * 2, -zoom * 2, zoom * 20});
-    lt.push_back({-zoom * 2, -zoom * 2, zoom * 20});
-    lt.push_back({-zoom * 2, zoom * 2, zoom * 20});
-    std::list<MyColor> ltC;
-    ltC.push_back({float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10});
-    ltC.push_back({float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10});
-    ltC.push_back({float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10, float(rand() % 10) / 10});
-    drawPull(lt, zoom*4, ltC);
-    glPopMatrix();
+    // 先画平面
+    glColor4f(1, 1, 1, 1);
+    glRectf(-width * zoom, -height * zoom, width * zoom, height * zoom);
 
     glFlush();
     glutSwapBuffers();
@@ -186,33 +231,22 @@ void myDisplay() {
  * 计时增加角度
  */
 void myIdle() {
-    static int m = 0;
-    m++;
-    if (m % 5000000 == 0) {
-        mySnake.updateSnake(nowDir);
-        if (mySnake.s.front().x == myFood.x &&
-            mySnake.s.front().y == myFood.y &&
-            mySnake.s.front().z == myFood.z) {
-            mySnake.eatFood(nowDir);
-            myFood.set(rand() % 50 - 25, rand() % 50 - 25, 1);
-        }
-        myDisplay();
-    }
+    myDisplay();
 }
 
 int main(int argc, char *argv[]) {
     srand((unsigned) time(NULL));
-    myFood.set(rand() % 50 - 25, rand() % 50 - 25, 1);
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(1000, 1000);
-    glutCreateWindow("太阳，地球和月亮");  // 改了窗口标题
+    glutCreateWindow("Demo");  // 改了窗口标题
 
     glutDisplayFunc(myDisplay);
     glutIdleFunc(myIdle);  // 表示在CPU空闲的时间调用某一函数
     glutSpecialFunc(processSpecialKeys);  // 按键
+    glutKeyboardFunc(processNormalKeys);
     glutReshapeFunc(changeSize);
     glutMouseFunc(mouse);
     glutMotionFunc(mouseMotion);
@@ -221,6 +255,14 @@ int main(int argc, char *argv[]) {
     // GL_DEPTH_TEST 用来开启更新深度缓冲区的功能
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
+//    const GLubyte *name = glGetString(GL_VENDOR);
+//    const GLubyte *biaoshifu = glGetString(GL_RENDERER);
+//    const GLubyte *OpenGLVersion = glGetString(GL_VERSION);
+//    const GLubyte *gluVersion = gluGetString(GLU_VERSION);
+//    std::cout << name << std::endl;
+//    std::cout << biaoshifu << std::endl;
+//    std::cout << OpenGLVersion << std::endl;
+//    std::cout << gluVersion << std::endl;
     glutMainLoop();
     return 0;
 }
--- myDrawUtil.h
+++ myDrawUtil.h
@@ -2,7 +2,6 @@
 #define OPENGLGAME_MYDRAWUTIL_H
 
 #include <list>
-#include <utility>
 
 template<class A>
 struct MyPos {
@@ -23,59 +22,23 @@ struct MyPos {
     MyPos(A x, A y, A z) : x(x), y(y), z(z) {}
 };
 
-struct MyColor {
-    float r{}, g{}, b{}, a{};
-
-    MyColor(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
-
-    MyColor() {
-        r = g = b = a = 1.0f;
-    };
+struct Per3dObject {
+    std::list<MyPos<float >> sketch;
+    float h = 0;
 };
 
-// 绘制正(长)方体,x,y,z为正(长)方体的距中心的距离
-void drawBox(float x, float y, float z, float r, float g, float b, float a) {
-    glBegin(GL_QUADS);
-
-    glColor4f(r, g, b, a);
-    // FRONT
-    glVertex3f(-x, -y, z);
-    glVertex3f(x, -y, z);
-    glVertex3f(x, y, z);
-    glVertex3f(-x, y, z);
-    // BACK
-    glVertex3f(-x, -y, -z);
-    glVertex3f(-x, y, -z);
-    glVertex3f(x, y, -z);
-    glVertex3f(x, -y, -z);
-    // LEFT
-    glVertex3f(-x, -y, z);
-    glVertex3f(-x, y, z);
-    glVertex3f(-x, y, -z);
-    glVertex3f(-x, -y, -z);
-    // RIGHT
-    glVertex3f(x, -y, -z);
-    glVertex3f(x, y, -z);
-    glVertex3f(x, y, z);
-    glVertex3f(x, -y, z);
-    // TOP
-    glVertex3f(-x, y, z);
-    glVertex3f(x, y, z);
-    glVertex3f(x, y, -z);
-    glVertex3f(-x, y, -z);
-    // BOTTOM
-    glVertex3f(-x, -y, z);
-    glVertex3f(-x, -y, -z);
-    glVertex3f(x, -y, -z);
-    glVertex3f(x, -y, z);
+void drawSketch(std::list<MyPos<float >> lt) {
+    /* 绘制线段操作 */
+    glBegin(GL_LINE_STRIP);
+    for (auto &item:lt) {
+        glVertex3f(item.x, item.y, item.z);
+    }
     glEnd();
 }
 
 // 拉伸操作lx,ly,lz为草图一系列点，h为高
-void drawPull(std::list<MyPos<float >> lt, float h, const std::list<MyColor> &ltC) {
-    auto iter = ltC.begin();
+void drawPull(std::list<MyPos<float >> lt, float h) {
     glBegin(GL_QUAD_STRIP);
-    glColor4f(iter->r, iter->g, iter->b, iter->a), ++iter;
     for (auto &item:lt) {
         glVertex3f(item.x, item.y, item.z);
         glVertex3f(item.x, item.y, item.z + h);
@@ -84,13 +47,11 @@ void drawPull(std::list<MyPos<float >> lt, float h, const std::list<MyColor> &lt
     // 相等表示闭合，绘制两个面加侧面，不相等绘制侧面
     if (lt.back() == lt.front()) {
         glBegin(GL_POLYGON);
-        glColor4f(iter->r, iter->g, iter->b, iter->a), ++iter;
         for (auto &item:lt) {
             glVertex3f(item.x, item.y, item.z);
         }
         glEnd();
         glBegin(GL_POLYGON);
-        glColor4f(iter->r, iter->g, iter->b, iter->a), ++iter;
         for (auto &item:lt) {
             glVertex3f(item.x, item.y, item.z + h);
         }
@@ -98,6 +59,45 @@ void drawPull(std::list<MyPos<float >> lt, float h, const std::list<MyColor> &lt
     }
 }
 
+void drawPull2(std::list<MyPos<float >> lt, float h) {
+    glBegin(GL_LINES);
+    for (auto &item:lt) {
+        glVertex3f(item.x, item.y, item.z);
+        glVertex3f(item.x, item.y, item.z + h);
+    }
+    glEnd();
+    glBegin(GL_LINES);
+    for (auto &item:lt) {
+        glVertex3f(item.x, item.y, item.z);
+    }
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+    for (auto &item:lt) {
+        glVertex3f(item.x, item.y, item.z + h);
+    }
+    glEnd();
+}
 
+// https://blog.csdn.net/sac761/article/details/52179585
+std::vector<double> screen2world(int x, int y) {
+    int viewport[4];
+    double modelview[16];
+    double projection[16];
+    float winX, winY, winZ;
+    double posX, posY, posZ;
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+    glGetDoublev(GL_PROJECTION_MATRIX, projection);
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    winX = (float) x;
+    winY = (float) viewport[3] - (float) y;
+    glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+    gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+    std::vector<double> v{4, posX, posY, posZ, 1.0};
+    return v;
+}
+
+inline float myRound(float a) {
+    return std::round(10 * a) / 10.0f;
+}
 
 #endif //OPENGLGAME_MYDRAWUTIL_H
